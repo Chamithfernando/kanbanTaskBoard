b7f41e313955d8b2555765588461bb1df635a0cf
Chamith <chameeefdo@gmail.com>
2021-03-02 12:25:05 +0530

    start

---
+++ src/main/java/io/kanban/kanbanTaskBoard/KanbanTaskBoardApplication.java
@@ -0,0 +1,13 @@
+package io.kanban.kanbanTaskBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanTaskBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanTaskBoardApplication.class, args);
+	}
+
+}
